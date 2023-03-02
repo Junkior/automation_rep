0ace92667d59ffd4ad255c32ac67d4f3cd417597
Junkior <punkissimple@gmail.com>
2023-03-02 18:02:06 +0300

    added shop test

--- book_store_testing/Shop.py
+++ book_store_testing/Shop.py
@@ -7,5 +7,54 @@ driver.maximize_window()
 driver.implicitly_wait(5)
 wait = WebDriverWait(driver, 10)
 
+###Отображение страницы товара
+
+# Откройте http://practice.automationtesting.in/
+driver.get("http://practice.automationtesting.in/")
+# Залогиньтесь
+my_account_btn = driver.find_element(By.ID, "menu-item-50")
+my_account_btn.click()
+login_email_field = driver.find_element(By.ID, "username")
+login_email_field.send_keys("oxample_test@gmail.com")
+login_password_field = driver.find_element(By.ID, "password")
+login_password_field.send_keys("_6Words_!")
+# Нажмите на вкладку "Shop"
+shop_menu_btn = driver.find_element(By.ID, "menu-item-40")
+shop_menu_btn.click()
+# Откройте книгу "HTML 5 Forms"
+html5_book_btn = driver.find_element(By.CSS_SELECTOR, ".masonry-done li:nth-child(3)>a>img")
+html5_book_btn.click()
+# Добавьте тест, что заголовок книги назвается: "HTML5 Forms"
+book_header = driver.find_element(By.TAG_NAME, "h1")
+book_header_txt = book_header.text
+assert book_header_txt == "HTML5 Forms"
+
+###
+
+### Количество товаров в категории
+
+# Откройте http://practice.automationtesting.in/
+driver.get("http://practice.automationtesting.in/")
+# Залогиньтесь
+my_account_btn = driver.find_element(By.ID, "menu-item-50")
+my_account_btn.click()
+login_email_field = driver.find_element(By.ID, "username")
+login_email_field.send_keys("oxample_test@gmail.com")
+login_password_field = driver.find_element(By.ID, "password")
+login_password_field.send_keys("_6Words_!")
+# Нажмите на вкладку "Shop"
+shop_menu_btn = driver.find_element(By.ID, "menu-item-40")
+shop_menu_btn.click()
+#Откройте категорию "HTML"
+html_category_btn = driver.find_element(By.CSS_SELECTOR, ".cat-item-19>a")
+html_category_btn.click()
+# Добавьте тест, что отображается три товара
+products = driver.find_elements(By.CSS_SELECTOR, ".masonry-done li")
+assert len(products) == 3
+
+
+
+
+
 
 
