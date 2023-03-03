829c501854ff2f381cd0cf5fcd72ce9e5db4f87c
Junkior <punkissimple@gmail.com>
2023-03-03 14:14:17 +0300

    more shop tests and steps look like test case

--- book_store_testing/login_registration.py
+++ book_store_testing/login_registration.py
@@ -11,18 +11,19 @@ wait = WebDriverWait(driver, 10)
 
 ### Регистрация аккаунта
 
-# Откройте http://practice.automationtesting.in/
+# Открыть http://practice.automationtesting.in/
 driver.get("http://practice.automationtesting.in/")
-# Нажмите на вкладку "My Account Menu"
+# Нажать на вкладку "My Account Menu"
 my_account_btn = driver.find_element(By.ID, "menu-item-50")
 my_account_btn.click()
-# В разделе "Register", введите email для регистрации
+# Ввести email для регистрации в разделе "Register"
 email_field = driver.find_element(By.ID, "reg_email")
 email_field.send_keys("oxample_test@gmail.com")
 # В разделе "Register", введите пароль для регистрации
+# Ввести password  для регистрации в разделе "Register"
 password_field = driver.find_element(By.ID, "reg_password")
 password_field.send_keys("_6Words_!")
-# Нажмите на кнопку "Register"
+# Нажать на кнопку "Register"
 registration_btn = driver.find_element(By.NAME, "register")
 registration_btn.click()
 ###
@@ -31,23 +32,26 @@ registration_btn.click()
 ### Логин в систему
 
 
-# Откройте http://practice.automationtesting.in/
+# Открыть http://practice.automationtesting.in/
 driver.get("https://practice.automationtesting.in/")
-# Нажмите на вкладку "My Account Menu"
+# Нажать на вкладку "My Account Menu"
 my_account_btn = driver.find_element(By.ID, "menu-item-50")
 my_account_btn.click()
 # В разделе "Login", введите email для логина
+# Ввести email для регистрации в разделе "Login"
 login_email_field = driver.find_element(By.ID, "username")
 login_email_field.send_keys("oxample_test@gmail.com")
 # В разделе "Login", введите пароль для логина
+# Ввести login для регистрации в разделе "Login"
 login_password_field = driver.find_element(By.ID, "password")
 login_password_field.send_keys("_6Words_!")
-# Нажмите на кнопку "Login"
+# Нажать на кнопку "Login"
 login_btn = driver.find_element(By.NAME, "login")
 login_btn.click()
-# Добавьте проверку, что на странице есть элемент "Logout"
+# Добавить проверку, что на странице есть элемент "Logout"
 logout_element_check = wait.until(
     EC.presence_of_element_located((By.CSS_SELECTOR, ".woocommerce-MyAccount-navigation>ul li:nth-child(6)>a"))
 )
 ###
+driver.quit()
 
